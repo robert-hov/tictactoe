e4104c189c50c3f14acc731b65656a1706500fc7
robert-hov <roberthvn88@gmail.com>
2022-02-23 12:44:53 +0400

    unfixed

--- src/Components/TicTacBoard.js
+++ src/Components/TicTacBoard.js
@@ -1,12 +1,29 @@
 import React, {useEffect, useState} from "react";
 import styled from "styled-components";
-import {click} from "@testing-library/user-event/dist/click";
 
 const TicTacBoard = ({size}) => {
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [xTurn, setXturn] = useState(true)
     const [xWins, setXWins] = useState(0)
     const [oWins, setOWins] = useState(0)
+    const [players, setPlayers] = useState('computer');
+
+    const TicTacItem = (i) => {
+        return (
+            <StyledTicTacItem
+                onClick={() => {
+                    if (winner()) return
+                    clickHandler(i)
+                }}
+            >
+                <StyledXO>
+                    {squares[i]}
+                </StyledXO>
+            </StyledTicTacItem>
+        )
+    }
+
+
 
     const winner = () => {
         const winners = [
@@ -39,46 +56,44 @@ const TicTacBoard = ({size}) => {
         return null
     }
 
-    const clickHandler = (i) => {
-        const cloneSquares = squares;
-
-        if (cloneSquares[i]) return
-        cloneSquares[i] = xTurn ? "x" : "o";
-
-        setXturn(!xTurn);
-        setSquares(cloneSquares)
-        winnerCounter();
+    const changePlayer = () => {
+        if(players === '2 players') setPlayers('computer')
+        else setPlayers('2 players')
     }
 
+    const clickHandler = (i) => {
+        const cloneSquares = squares;
+        if (cloneSquares[i]) return
+        cloneSquares[i] = "x";
+        if(players === '2 players') setXturn(true);
+
+        if(players === 'computer') {
+            let randomI = 0;
+            while (true) {
+                randomI = Math.ceil(Math.random() * 8)
+                console.log(randomI)
+                if (!cloneSquares[randomI] && randomI !== 10) break;
+            }
+
+            cloneSquares[randomI] = "o";
+        }
+        setSquares(cloneSquares)
+    }
+
+    // useEffect(() => {
+    //     winner();
+    //     draw();
+    // }, [squares])
 
 
     const winnerCounter = () => {
-        if(!winner()) return
-        if(winner() === 'x') setXWins(xWins + 1)
-        if(winner() === 'o') setOWins(oWins + 1)
+        if (!winner()) return
+        if (winner() === 'x') setXWins(xWins + 1)
+        if (winner() === 'o') setOWins(oWins + 1)
     }
-
-    const TicTacItem = (i) => {
-        return (
-            <>
-                <StyledTicTacItem
-                    data-num={i}
-                    onClick={() => {
-                        if (winner()) return
-                        clickHandler(i)
-                    }}
-                >
-                    <StyledXO>
-                        {squares[i]}
-                    </StyledXO>
-                </StyledTicTacItem>
-            </>
-        )
-    }
-
     return (
         <>
-            {!draw() && !winner() && <StyledInfo>Now {xTurn ? 'x' : 'o'} turn</StyledInfo>}
+            {/*{!draw() && !winner() && <StyledInfo>Now {xTurn ? 'x' : 'o'} turn</StyledInfo>}*/}
             {winner() && <StyledWinner>Winner is {winner()}</StyledWinner>}
             {draw() && <StyledDraw>It's a draw</StyledDraw>}
             <StyledTicTacContainer size={size}>
@@ -92,10 +107,11 @@ const TicTacBoard = ({size}) => {
                 {TicTacItem(7)}
                 {TicTacItem(8)}
             </StyledTicTacContainer>
-            <StyledReset onClick={() => {
+            <StyledBtn onClick={() => {
                 setSquares(Array(9).fill(null))
-            }}>Reset</StyledReset>
+            }}>Reset</StyledBtn>
             <StyledWinners>X: {xWins} | O: {oWins} </StyledWinners>
+            <StyledBtn onClick={changePlayer}>{players}</StyledBtn>
         </>
     )
 }
@@ -139,11 +155,10 @@ const StyledXO = styled.div`
   font-size: 1.5rem;
 `
 
-const StyledReset = styled.button`
+const StyledBtn = styled.button`
   margin-top: 0.5rem;
 `
 
 const StyledWinners = styled.div`
   font-family: 'Akaya Telivigala', cursive;
-
 `
